9d02b498b838f10a040674cb24158caa2a2af865
Casey's Laptop <caseykey@cs.colostate.edu>
2020-02-17 23:55:42 -0700

    removed unnecessary comments and added comments for user-content

--- examples/community/ipfs-music-player/src/App.js
+++ examples/community/ipfs-music-player/src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import Background from './Background'
 import AlbumArt from './AlbumArt'
 import './style.css';
-import albumCover from "./media/vultures.png";
-import Vultures from './media/Vultures.mp3';
 import AudioPlayer from './AudioPlayer'
 import { FaCode } from 'react-icons/fa'
 
+// Here is an example of importing a song and its cover art
+import albumCover from "./media/vultures.png";
+import Vultures from './media/Vultures.mp3';
+
 function App() {
   let media = {
+      // Here you can change your song's title, the artist, and set sources
       title: 'Vultures',
       artist: 'Pervsydell',
       sources: {
+        // Set source by importing the music file via relative path
+        // Supported formats: mp3, m4a, oga, flac, wav, flv, and more!
         m4a: Vultures,
       },
       free: true,
@@ -20,7 +25,10 @@ function App() {
   return (
     <div className="App">
       <header>
+          { /* Feel free to change IPFS Music Player to your own text */ }
           <h1 className="my-auto">IPFS Music Player</h1>
+          { /* You may remove this attribution. Contact me for more features */ }
+          { /* Additional paid features include: visualizer, tipping and revenue splitting via crypto*/ }
           <a href="https://www.caseyjkey.com/" className="my-auto"><FaCode /> <span>by Casey Key</span></a>
       </header>
       <Background />
