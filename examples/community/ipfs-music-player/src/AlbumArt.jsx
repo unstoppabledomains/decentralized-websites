f44e317e385f745fbf739ac0cc0c75d7ca0c2036
Casey Key <caseykey@rams.colostate.edu>
2020-03-20 23:09:47 -0600

    used CSS for image centering and gradient

--- examples/community/ipfs-music-player/src/AlbumArt.jsx
+++ examples/community/ipfs-music-player/src/AlbumArt.jsx
@@ -1,19 +1,9 @@
-import React, { useEffect, useState } from "react";
-// We almost just want this to be passed in a image object or url and then send out an http request to 
-// go retreve and render. In the encap player component. 
+import React from "react";
 function AlbumArt({ image }) {
 
-    const [centerX, setCenterX] = useState(window.innerWidth / 2);
-    const [centerY, setCenterY] = useState((window.innerHeight / 2) - 75);
-    useEffect(() => {
-        setCenterX(window.innerWidth / 2);
-        setCenterY((window.innerHeight / 2) - 75);
-    }, [centerX, centerY]);
-    
-
     const styles = {
-        left: centerX - 150,
-        top: centerY - 100
+        left: `calc(50vw - 150px)`,
+        top: `calc(50vh - 175px)`
     };
 
     return(
--- examples/community/ipfs-music-player/src/Background.jsx
+++ examples/community/ipfs-music-player/src/Background.jsx
@@ -1,28 +1,14 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 
 export default function Background() {
-  function setGradient() {
-    // set to the size of device
-    let canvas = document.getElementById("gradient");
-    let ctx = canvas.getContext("2d");
-    
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight - 6;
 
-    let gradient = ctx.createLinearGradient(0,0,0,canvas.height);
-    gradient.addColorStop(0,"rgba(35, 7, 77, 1)");
-    gradient.addColorStop(1,"rgba(204, 83, 51, 1)");
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-  }
-
-
-  // Set the background to a gradient when component mounts
-  useEffect(() => {
-    setGradient();
-  }, []);
+  const styles = {
+    background: `linear-gradient(rgba(35, 7, 77, 1), rgba(204, 83, 51, 1))`,
+    width: `100vw`,
+    height: `100vh`  
+  };
   
   return (
-    <canvas id="gradient" />
+    <canvas id="gradient" style={ styles } />
   );
 }
